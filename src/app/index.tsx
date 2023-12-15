a22e04284bd435809e822de9baf93f0a44f42d6b
AnnaShpak01 <elistana@i.ua>
2023-12-15 19:57:17 +0100

    comment index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import { Provider } from 'react-redux'
 
-import App from "./components/app/App";
-import store from "./store";
+// import App from './components/app/App'
+// import store from './store'
 
-import "./styles/index.scss";
+// import './styles/index.scss'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// // Перевірка, чи код виконується в браузері перед викликом createRoot
+// if (typeof document !== 'undefined') {
+//   const root = ReactDOM.createRoot(document.getElementById('root'))
+
+//   root.render(
+//     <React.StrictMode>
+//       <Provider store={store}>
+//         <App />
+//       </Provider>
+//     </React.StrictMode>
+//   )
+// }
