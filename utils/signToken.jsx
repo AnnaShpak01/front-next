c7c949b884dbe999b2d6334549de46df9f39f623
AnnaShpak01 <elistana@i.ua>
2024-03-29 19:22:49 +0100

    changed period of expire for jwt token

--- utils/signToken.jsx
+++ utils/signToken.jsx
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 const SignToken = async (email) => {
   const token = await jwt.sign({ id: email }, process.env.NEXTAUTH_SECRET, {
-    expiresIn: '1m',
+    expiresIn: '1d',
   })
   return token
 }
